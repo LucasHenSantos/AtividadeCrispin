9ed179207027a85b42ef9c8e24f8828671f7cb68
LucasHenSantos <Lucas.henrique@aluno.imepac.edu.br>
2024-08-29 06:41:42 -0300

    "Atualização"

--- AtividadeCrispin/src/main/java/br/edu/imepac/Main.java
+++ AtividadeCrispin/src/main/java/br/edu/imepac/Main.java
@@ -4,11 +4,9 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new
-                BigDecimal("1120.98"), "ICMS_SP");
+        Orcamento orcamento = new Orcamento(new BigDecimal("1120.98"), "ICMS_MG");
         IcmsFactory icmsFactory = new IcmsFactory();
-        BigDecimal resultado = icmsFactory.IcmsPorEstado(
-                "ICMS_SP").calculoRegiao(orcamento);
+        BigDecimal resultado = icmsFactory.IcmsPorEstado("ICMS_MG").calculoRegiao(orcamento);
         System.out.println("Icms = " + resultado);
     }
 }
